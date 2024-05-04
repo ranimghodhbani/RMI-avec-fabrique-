4ffc3db44dc7aeafda705b78676cff16fa6d95aa
ranimghodhbani <159330924+ranimghodhbani@users.noreply.github.com>
2024-05-04 19:12:58 +0100

    Create ReverseInterface.java

---
+++ Reverse/Server/ReverseInterface.java
@@ -0,0 +1,9 @@
+package Tprmiavecfabrique.Reverse.Server;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public interface ReverseInterface extends Remote 
+{
+	String reverseString(String chaine) throws RemoteException;
+}
